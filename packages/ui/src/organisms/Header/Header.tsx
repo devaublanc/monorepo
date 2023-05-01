7e0b0d8de8ea3a486e408b0b70cf6f570c10a3c4
Benjamin <benjamin.devaublanc@gmail.com>
2023-05-01 15:18:59 +0200

    install next.js app

--- packages/ui/src/organisms/Header/Header.tsx
+++ packages/ui/src/organisms/Header/Header.tsx
@@ -7,12 +7,12 @@ type User = {
   name: string;
 };
 
-interface HeaderProps {
+export type HeaderProps = {
   user?: User;
   onLogin: () => void;
   onLogout: () => void;
   onCreateAccount: () => void;
-}
+};
 
 export const Header = ({
   user,
