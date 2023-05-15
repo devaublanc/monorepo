bc3695b968a2783527ecd3028b69a6b9e217ebcf
Benjamin <benjamin.devaublanc@gmail.com>
2023-05-15 15:01:31 +0200

    fix test

--- apps/web-app/src/pages/__tests__/Home.test.tsx
+++ apps/web-app/src/pages/__tests__/Home.test.tsx
@@ -1,9 +1,11 @@
-import { expect, test } from "vitest";
+import { describe, expect, it } from "vitest";
 import { render, screen, within } from "@testing-library/react";
 import Home from "..";
 
-test("home", () => {
-  render(<Home />);
-  const main = within(screen.getByRole("main"));
-  expect(main.getByText("Hello world", { selector: "h1" })).toBeDefined();
+describe("home", () => {
+  it("should render", () => {
+    render(<Home />);
+    const main = within(screen.getByRole("main"));
+    expect(main.getByText("Hello world", { selector: "h1" })).toBeDefined();
+  });
 });
